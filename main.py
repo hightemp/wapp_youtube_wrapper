3266b6ab8954ad1f9430bbb1822f0895aa530ad0
Панов Антон <hightemp.unknown@gmail.com>
2022-12-25 10:26:39 +0300

    Sun Dec 25 10:26:39 AM MSK 2022 update

--- main.py
+++ main.py
@@ -30,8 +30,35 @@ def get_db():
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
+        sSQL = """
+DROP TABLE IF EXISTS youtube_search_words;
+CREATE TABLE youtube_search_words (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS youtube_found;
+CREATE TABLE youtube_found (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    youtube_search_words_id INTEGER NULL,
+    youtube_id TEXT NOT NULL,
+    url TEXT NULL
+);
+
+DROP TABLE IF EXISTS update_queue;
+CREATE TABLE update_queue (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at INTEGER NULL,
+    started_at INTEGER NULL,
+    stopped_at INTEGER NULL,
+    update_status INTEGER NULL,
+    youtube_search_words_id INTEGER NULL
+);
+
+        """
+        # with app.open_resource('schema.sql', mode='r') as f:
+        #    db.cursor().executescript(f.read())
+        db.cursor().executescript(sSQL)
         db.commit()
 
 @app.teardown_appcontext
