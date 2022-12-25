cbd0bed91bb74cd767438b4c2d2d82ce2806cea1
Панов Антон <hightemp.unknown@gmail.com>
2022-12-25 10:23:22 +0300

    Sun Dec 25 10:23:22 AM MSK 2022 update

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-CFILE=wapp_youtube_wrapper.pyz
+CFILE=wapp_youtube_wrapper
 
 timestamp=$(date +%s)
 VERSION=$(echo `cat VERSION`.$timestamp)
@@ -6,7 +6,7 @@ VERSION=$(echo `cat VERSION`.$timestamp)
 git add .
 git commit -am "`date` update"
 git tag $VERSION
-git push
+git push --force
 
 if [ "$?" != "0" ]; then
     echo "====================================================="
@@ -15,10 +15,12 @@ if [ "$?" != "0" ]; then
     exit 1
 fi
 
+echo "====================================================="
 cd ..
-python3 -m zipapp wapp_youtube_wrapper -p "/usr/bin/env python3"
-mv wapp_youtube_wrapper.pyz ./wapp_youtube_wrapper
-cd wapp_youtube_wrapper
+python3 -m zipapp $CFILE -p "/usr/bin/env python3"
+cd $CFILE
+echo "====================================================="
 
-echo gh release create $VERSION -t $VERSION -n '""' $CFILE
-gh release create $VERSION -t $VERSION -n "" $CFILE
+gh release create $VERSION -t $VERSION -n "" --target main 
+gh release upload $VERSION ../$CFILE.pyz --clobber
+
