3b85f2c0ab43ff92abe0444c38b79679a123d2e0
Панов Антон <hightemp.unknown@gmail.com>
2022-12-25 10:16:28 +0300

    Sun Dec 25 10:16:28 AM MSK 2022 update

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-CFILE=wapp_youtube_wrapper.pyz
+CFILE=wapp_youtube_wrapper
 
 timestamp=$(date +%s)
 VERSION=$(echo `cat VERSION`.$timestamp)
@@ -16,9 +16,10 @@ if [ "$?" != "0" ]; then
 fi
 
 cd ..
-python3 -m zipapp wapp_youtube_wrapper -p "/usr/bin/env python3"
-mv wapp_youtube_wrapper.pyz ./wapp_youtube_wrapper
-cd wapp_youtube_wrapper
+python3 -m zipapp $CFILE -p "/usr/bin/env python3"
+mv $CFILE.pyz ./$CFILE
+cd $CFILE
 
-echo gh release create $VERSION -t $VERSION -n '""' $CFILE
-gh release create $VERSION -t $VERSION -n "" $CFILE
+gh release create $VERSION -t $VERSION -n "" 
+gh release upload $VERSION ../$CFILE.pyz --clobber
+
